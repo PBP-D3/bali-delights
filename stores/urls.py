22be59258daae4a85b6e36b435f35bd414bb908c
Scallss <pascalhafidz2005@gmail.com>
2024-10-26 09:20:00 +0700

    feat:added stores views and simple templates

--- stores/urls.py
+++ stores/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from stores.views import show_stores
+from stores.views import *
 
 app_name = 'stores'
 
 urlpatterns = [
     path('', show_stores, name='show_stores'),
+    path('register/', register_store, name='register_store'),
+    path('<int:id>/', show_store_details, name='show_store_details'),
+    path('my-store/', show_user_store, name='show_user_store'),
+    path('register-store/', register_store, name='register_store'),
+    path('edit/<int:id>/', edit_store, name='edit_store'),
+    path('delete/<int:id>/', delete_store, name='delete_store'),
+    path('owner/<int:id>/', owner_store_view, name='owner_store_view')
 ]
-
--- stores/views.py
+++ stores/views.py
@@ -1,13 +1,86 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Store
+from .forms import StoreForm
+from products.models import Product
+from django.core import serializers
 
-# Create your views here.
-# Create your views here.
 def show_stores(request):
-    context = {
-        'npm' : '2306220444',
-        'name': request.user.username,
-        'class': 'PBP D',
-        'last_login': request.COOKIES['last_login'],
-    }
+    stores = Store.objects.all()
+    return render(request, 'stores.html', {'stores': stores})
 
-    return render(request, "stores.html", context)
+@login_required(login_url="/login")
+def show_user_store(request):
+    # Get the current user
+    user = request.user
+    
+    # Retrieve the store owned by the user
+    stores = Store.objects.filter(owner_id=user.id)
+    print(stores)
+    
+    return render(request, 'user_store.html', {'stores': stores})
+
+@login_required(login_url="/login")
+def register_store(request):
+    if request.method == 'POST':
+        form = StoreForm(request.POST)
+        if form.is_valid():
+            store = form.save(commit=False)
+            store.owner_id = request.user
+            store.save()
+            return redirect('stores:show_user_store')  # Redirect to the store listing page after registration
+    else:
+        form = StoreForm()
+    
+    return render(request, 'register_store.html', {'form': form})
+
+@login_required(login_url="/login")
+def edit_store(request, id):
+    store = get_object_or_404(Store, id=id)
+    if request.method == 'POST':
+        form = StoreForm(request.POST, instance=store)
+        if form.is_valid():
+            form.save()
+            return redirect('stores:show_user_store')
+    else:
+        form = StoreForm(instance=store)
+
+    return render(request, 'edit_store.html', {'form': form, 'store': store})
+
+@login_required
+def delete_store(request, id):
+    store = get_object_or_404(Store, pk=id)
+
+    store.delete()
+    return redirect('stores:show_user_store')
+    
+
+def show_store_details(request, id):
+    store = get_object_or_404(Store, pk=id)
+    products = Product.objects.filter(store_id=store)
+    return render(request, 'show_store_details.html', {'store': store, 'products': products})
+
+def owner_store_view(request, id):
+    store = get_object_or_404(Store, pk=id)
+    products = Product.objects.filter(store_id=store)
+    return render(request, 'owner_store_view.html', {'store': store, 'products': products})
+
+def show_xml(request):
+    data = Store.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Store.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Store.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Store.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+
