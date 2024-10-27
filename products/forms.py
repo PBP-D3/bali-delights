18383a8f85393bbbc1464d87995c1d0f7f01e19c
m.azzam <m.azzam.azis@gmail.com>
2024-10-27 23:51:40 +0700

    chore: rm env and db

--- products/forms.py
+++ products/forms.py
@@ -4,12 +4,12 @@ from products.models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "description", "price", "stock", "category", "photo_upload", "photo"]
+        fields = ["name", "description", "price", "stock", "category", "photo_upload", "photo_url"]
         
     def clean_photo_upload(self):
         photo_upload = self.cleaned_data.get('photo_upload')
         return photo_upload
 
-    def clean_photo(self):
-        photo = self.cleaned_data.get('photo')
-        return photo
+    def clean_photo_url(self):
+        photo_url = self.cleaned_data.get('photo_url')
+        return photo_url
