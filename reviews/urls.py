c9b089281e747da35edca0bf3e73d241bdf42d90
Edmond Christian <edmond.chr@gmail.com>
2024-10-27 13:46:12 +0700

    feat: top reviews page; style: spacing on multiple pages

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,23 +1,18 @@
 from django.urls import path
-from reviews.views import show_reviews, create_review, edit_review, delete_review, product_reviews, show_xml, show_json, user_reviews, toggle_like
+from reviews.views import show_reviews, create_review, edit_review, delete_review, product_reviews, show_xml, show_json, user_reviews, toggle_like, show_reviews
 
 app_name = 'reviews'
 
 urlpatterns = [
     path('', show_reviews, name='show_reviews'),
-    # path('create-review', create_review, name='create_review'),
     path('edit-review/<int:review_id>', edit_review, name='edit_review'),
-    # path('delete-review/<int:review_id>', delete_review, name='delete_review')
     path('<int:product_id>', product_reviews, name='product_reviews'),
     path('<int:product_id>/create-review/', create_review, name='create_review'),
-    # path('product/<int:product_id>/reviews/', product_reviews, name='product_reviews'),
-    # path('product/<int:product_id>/create_review/', create_review, name='create_review'),
     path('<int:product_id>/xml', show_xml, name = 'show_xml'),
     path('<int:product_id>/json', show_json, name = 'show_json'),
     path('my-review/', user_reviews, name='user_reviews'),
     path('edit-review/<int:review_id>/', edit_review, name='edit_review'),
     path('delete-review/<int:review_id>/', delete_review, name='delete_review'),
     path('toggle-like/<int:review_id>/', toggle_like, name='toggle_like'),
-
 ]
 
