781d5735ce0aab574a463b4a316224eb63fe4201
Edmond Christian <edmond.chr@gmail.com>
2024-12-02 16:13:39 +0700

    added json endpoint for review module and top reviews

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main.views import show_main
-from main.views import register, login_user, logout_user
+from main.views import register, login_user, logout_user, top_reviews_json
 
 
 app_name = 'main'
@@ -13,5 +13,6 @@ urlpatterns = [
     path('login', login_user, name='login'),
     
     path('logout', logout_user, name='logout'),  
-    
+    path('top-reviews-json/', top_reviews_json, name='top_reviews_json'),
+
 ]
